e934c6103cc56f12a34703f5f22df1c05a48a39c
Sameer Prajapati <sameerprajapati066@gmail.com>
2025-06-28 23:41:36 +0530

    Update activity_analysis.py

--- Basic Codes/activity_analysis.py
+++ Basic Codes/activity_analysis.py
@@ -1,13 +1,8 @@
 
-"""
-Create dataset of your life at VIT Bhopal campus in the span of
-24hrs for 2 days.
-"""
-
 import matplotlib.pyplot as plt
 
-categories = ['Sleep', 'Classes', 'Eating', 'Exercise', 'Studying', 'Free Time']
-hours = [14, 9, 4, 2, 13, 6]
+categories = ['Sleep', 'Classes', 'Eating', 'Studying', 'Free Time']
+hours = [14, 9, 4, 13, 6]
 
 plt.bar(categories, hours, color=['blue', 'green', 'orange', 'red', 'purple', 'brown'])
 plt.title('Time Spent in Different Activities Over 2 Days')
